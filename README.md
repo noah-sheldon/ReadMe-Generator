6e9417e77a5c35af255190ef45eacf09f4b74978
Noah Sheldon <noahsheldon06@gmail.com>
2024-01-16 18:45:52 +0000

    adding index.js with prompts and readme generator code

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# ReadMe-Generator
+
+# ReadMe Generator
+
+## Description
+This project helps create a readme automatically
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+Clone the repo, install inquirer, then run using node index.js
+
+## Usage
+Run the application using node index.js
+
+## License
+![MIT License](https://img.shields.io/badge/license-MIT-green)
+
+This application is covered under the MIT license.
+
+## Contributing
+Contribute through the issues tab on github
+
+## Tests
+The repo doesn't consist of tests at this moment.
+
+## Questions
+For any questions, please contact:
+- GitHub: [noah-sheldon](https://github.com/noah-sheldon)
+- Email: noahsheldon06@gmail.com
+  
--- index.js
+++ index.js
@@ -1,21 +1,103 @@
-const fs = require("fs");
-const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-// array of questions for user
-const questions = [
+// Function to generate README content based on user input
+function generateREADME(answers) {
+  return `
+# ${answers.title}
 
-];
+## Description
+${answers.description}
 
-// function to write README file
-function writeToFile(fileName, data) {
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+![${answers.license} License](https://img.shields.io/badge/license-${encodeURIComponent(answers.license)}-green)
+
+This application is covered under the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+For any questions, please contact:
+- GitHub: [${answers.github}](https://github.com/${answers.github})
+- Email: ${answers.email}
+  `;
 }
 
-// function to initialize program
-function init() {
+// Inquirer prompts
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Enter the title of your project:',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a short description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Enter usage information:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your application:',
+      choices: ['MIT', 'Apache', 'GPL', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Enter contribution guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Enter test instructions:',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+  ])
+  .then((answers) => {
+    // Generate README content
+    const readmeContent = generateREADME(answers);
 
-}
+    // Write README to file
+    fs.writeFileSync('README.md', readmeContent);
 
-// function call to initialize program
-init();
+    console.log('README.md generated successfully!');
+  })
+  .catch((err) => console.error(err));
